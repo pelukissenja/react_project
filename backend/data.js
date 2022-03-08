1156013a1b49225568dfa0b443bd58092b609cbe
pelukissenja <hasan32@guru.smp.belajar.id>
2022-03-08 10:31:38 +0700

    backend

---
+++ backend/data.js
@@ -0,0 +1,77 @@
+const data = {
+        products:[
+            {
+                _id: '1',
+                name: 'Nike Shoes',
+                category: 'shoes',
+                image: '../images/1.jpg',
+                price: 120,
+                countInStock : 102,
+                brand:'NIKE',
+                rating: 2,
+                numReviews: 6,
+                description:'This shoes never been bought!'
+            },
+            {
+                _id: '2',
+                name: 'Adidas Shoes',
+                category: 'shoes',
+                image: '../images/2.jpg',
+                price: 420,
+                countInStock : 10,
+                brand:'ADIDAS',
+                rating: 3,
+                numReviews: 5,
+                description:'This shoes never been bought!'
+            },
+            {
+                _id: '3',
+                name: 'ATT Shoes',
+                category: 'shoes',
+                image: '../images/3.jpg',
+                price: 320,
+                countInStock : 2,
+                brand:'ATT',
+                rating: 1.5,
+                numReviews: 7,
+                description:'This shoes never been bought!'
+            },
+            {
+                _id: '4',
+                name: 'Converse Shoes',
+                category: 'shoes',
+                image: '../images/4.jpg',
+                price: 220,
+                countInStock : 9,
+                brand:'CONVERSE',
+                rating: 2.3,
+                numReviews: 9,
+                description:'This shoes never been bought!'
+            },
+            {
+                _id: '5',
+                name: 'Hakim Shoes',
+                category: 'shoes',
+                image: '../images/5.jpg',
+                price: 100,
+                countInStock : 112,
+                brand:'HAKIM',
+                rating: 2.5,
+                numReviews: 4,
+                description:'This shoes never been bought!'
+            },
+            {
+                _id: '6',
+                name: 'Ihsan Shoes',
+                category: 'shoes',
+                image: '../images/6.jpg',
+                price: 110,
+                countInStock : 0,
+                brand:'IHSAN',
+                rating: 3.75,
+                numReviews: 2,
+                description:'This shoes never been bought!'
+            },
+        ],
+};
+export default data;
